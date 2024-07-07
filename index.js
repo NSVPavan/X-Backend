fe86333439ce58a8077c208d7d0d866f58d0c695
Naramala siva venkata pavan <sivavenkatapavan@gmail.com>
2024-07-07 15:23:26 +0530

    Fix: Configure view engine directory
    - Added app.set('views', path.join(__dirname, "views"));

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 let app = express();
 
 const port = 3000;
 
 app.set('view engine','ejs');
+app.set('views',path.join(__dirname,"views"));
+
 app.listen(port,()=>{
     console.log(`Listening on port: ${port}`);
 });
