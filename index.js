c38c7211d843b34cfdb0925c57d5ef4024224e9e
Naramala siva venkata pavan <sivavenkatapavan@gmail.com>
2024-07-07 15:45:05 +0530

    Render all posts at /posts route using ejs

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ let app = express();
 
 const port = 3000;
 
+let posts=[
+    {
+        username: "Elon Musk",
+        content: "I own twitter now... Oh! I mean X"
+    },
+    {
+        username: "Elon Musk",
+        content: "It's me again. Now you can get verified just by paying"
+    },
+    {
+        username: "Mark Zuckerberg",
+        content: "We are launching threads!!"
+    }
+];
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,"views"));
 
@@ -12,5 +26,5 @@ app.listen(port,()=>{
 });
 
 app.get('/posts',(req,res)=>{
-    res.send('Handling get request');
+    res.render('index.ejs',{posts});
 })
