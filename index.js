1fd1bc92da80ea16d19893523f297f11ea9ad915
Naramala siva venkata pavan <sivavenkatapavan@gmail.com>
2024-07-09 20:02:03 +0530

    Add unique id for each post

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const exp = require('constants');
 const express = require('express');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 let app = express();
 
 const port = 3000;
 
 let posts=[
     {
+        id: uuidv4(),
         username: "Elon Musk",
         content: "I own twitter now... Oh! I mean X"
     },
     {
+        id: uuidv4(),
         username: "Elon Musk",
         content: "It's me again. Now you can get verified just by paying"
     },
     {
+        id: uuidv4(),
         username: "Mark Zuckerberg",
         content: "We are launching threads!!"
     }
@@ -39,6 +43,7 @@ app.get('/posts/new',(req,res)=>{
 
 app.post('/posts',(req,res)=>{
     let {username,content} = req.body;
-    posts.push({username,content});
+    let id = uuidv4();
+    posts.push({username,content,id});
     res.redirect('/posts');
 })
